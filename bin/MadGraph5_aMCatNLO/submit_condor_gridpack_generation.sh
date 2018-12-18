1ff292847a1886828f08b0434cf649b292555004
Kevin Siehl <kevin.siehl@wayne.edu>
2018-12-18 14:19:31 -0600

    updating so i can change branches

--- bin/MadGraph5_aMCatNLO/submit_condor_gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/submit_condor_gridpack_generation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source Utilities/source_condor.sh
 
-name=$1
-carddir=$2
+name="WW_aTGC_reweight" #$1
+carddir="cards/examples/WW_aTGC_reweight" #$2
 workqueue="condor"
 scram_arch=$3
 cmssw_version=$4
